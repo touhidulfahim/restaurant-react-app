38a7a1f5ee14bd9d99d38f62b30eeeec95eb0fa7
Mohammad Touhidul Islam <muhdtohidul@gmail.com>
2021-05-01 23:46:42 +0600

    Footer component

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div></div>
+        <div className="footer" >
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-auto">
+                        <p>Restaurant App &#169; Copyright {new Date().getFullYear()} </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
